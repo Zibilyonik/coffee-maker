a59e3f44f811a02b7fd46edcad252007569c7631
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-02-26 14:48:45 +0100

    init commit, added basic printing functions

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// write your code here
+	fmt.Println("Starting to make a coffee")
+	fmt.Println("Grinding coffee beans")
+	fmt.Println("Boiling water")
+	fmt.Println("Mixing boiled water with crushed coffee beans")
+	fmt.Println("Pouring coffee into the cup")
+	fmt.Println("Pouring some milk into the cup")
+	fmt.Println("Coffee is ready!")
+}
